c97be645f4067a635b83f716b322e88b7a504bb4
Kenji Konno <konno.kenji@gmail.com>
2025-05-02 16:58:32 +0900

    502sb

--- Karpathy/llm/bigram.py
+++ Karpathy/llm/bigram.py
@@ -12,6 +12,8 @@ eval_interval = 300
 learning_rate = 1e-2
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
+n_embd = 32
+#-------------
 
 torch.manual_seed(1337)
 
@@ -58,15 +60,20 @@ def estimate_loss():
 # super simple bigram model
 class BigramLanguageModel(nn.Module):
 
-    def __init__(self, vocab_size):
+    def __init__(self):
         super().__init__()
         # each token directly reads off the logits for the next token from a lookup table
-        self.token_embedding_table = nn.Embedding(vocab_size, vocab_size)
+        self.token_embedding_table = nn.Embedding(vocab_size, n_embd)
+        self.position_embedding_table = nn.Embedding(block_size, n_embd)
+        self.lm_head = nn.Linear(n_embd, vocab_size)
 
     def forward(self, idx, targets=None):
 
         #idx and targets are both (B, T) tensor of integers
-        logits = self.token_embedding_table(idx)  #(B, T, C) batch, block, channel or vocab
+        tok_emb = self.token_embedding_table(idx)  #(B, T, C) batch, block, channel or vocab
+        pos_emb = self.position_embedding_table(torch.arange(T, device=device))  #(T, C)
+        x = tok_emb + pos_emb  #(B, T, C)
+        logits = self.lm_head(x)  #(B, T, C)  but C is vocab_size channel
 
         if targets is None:
             loss = None
@@ -99,7 +106,7 @@ class BigramLanguageModel(nn.Module):
             # print("idx_next", idx_next)
         return idx
 
-model = BigramLanguageModel(vocab_size)
+model = BigramLanguageModel()
 m = model.to(device)
 
 # create a pytorch optimiser
