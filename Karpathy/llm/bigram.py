6465272ed3e6f63ff89b9743ad38a40313cfbe46
kenji <konno.kenji@gmail.com>
2025-05-04 23:37:05 +0900

    cad2504

--- Karpathy/llm/bigram.py
+++ Karpathy/llm/bigram.py
@@ -5,16 +5,16 @@ import matplotlib.pyplot as plt
 
 
 #hyper parameters
-batch_size = 64
-block_size = 256
+batch_size = 8 #64
+block_size = 4 #256
 max_iters = 5000
-eval_interval = 500
-learning_rate = 3e-4
+eval_interval = 250
+learning_rate = 3e-5
 device = 'cuda' if torch.cuda.is_available() else 'cpu'
 eval_iters = 200
-n_embd = 384
-n_head = 6
-n_layer = 6
+n_embd = 256 #384
+n_head = 4  #6
+n_layer = 4  #6
 dropout = 0.2
 #-------------
 
