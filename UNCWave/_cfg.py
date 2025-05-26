6a6368c951d3bf027dab4e9951d4e9c15f80758a
konno <kkonno@soliton-cm.com>
2025-05-26 23:35:59 +0900

    526

--- UNCWave/_cfg.py
+++ UNCWave/_cfg.py
@@ -1,20 +1,26 @@
 
 from types import SimpleNamespace
 import torch
+import glob
 
 cfg= SimpleNamespace()
 cfg.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 cfg.local_rank = 0
 cfg.seed = 123
-cfg.subsample = None
+cfg.subsample = 20 #None
+data_paths = sorted(glob.glob("./datasetfiles/FlatVel_A/data/*.npy"))
+label_paths = sorted(glob.glob("./datasetfiles/FlatVel_A/model/*.npy"))
+cfg.file_pairs = list(zip(sorted(glob.glob("./datasetfiles/FlatVel_A/data/*.npy")), sorted(glob.glob("./datasetfiles/FlatVel_A/model/*.npy"))))
+# cfg.file_pairs = list(zip(data_paths, label_paths))
 
 cfg.backbone = "convnext_small.fb_in22k_ft_in1k"
 cfg.ema = True
 cfg.ema_decay = 0.99
 
 cfg.epochs = 1
-cfg.batch_size = 16
-cfg.batch_size_val = 16
+cfg.batch_size = 8  # 16
+cfg.batch_size_val = 8 # 16
 
 cfg.early_stopping = {"patience": 3, "streak": 0}
-cfg.logging_steps = 100
+cfg.logging_steps = 10
+
