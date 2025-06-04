29e707c5dbf92f9f3858c3d63375f6eb789e89b4
kenji <konno.kenji@gmail.com>
2025-06-05 00:18:03 +0900

    64

--- UNCWave/_cfg.py
+++ UNCWave/_cfg.py
@@ -8,11 +8,11 @@ cfg.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 cfg.local_rank = 0
 cfg.seed = 123
 cfg.subsample = 100 #None
-data_paths = sorted(glob.glob("./datasetfiles/FlatVel_A/data/*.npy"))
-label_paths = sorted(glob.glob("./datasetfiles/FlatVel_A/model/*.npy"))
+
 cfg.file_pairs = list(zip(sorted(glob.glob("./datasetfiles/FlatVel_A/data/*.npy")), sorted(glob.glob("./datasetfiles/FlatVel_A/model/*.npy"))))
 # cfg.file_pairs = list(zip(data_paths, label_paths))
-
+data_paths = sorted(glob.glob("./datasetfiles/FlatVel_A/data/*.npy"))
+label_paths = sorted(glob.glob("./datasetfiles/FlatVel_A/model/*.npy"))
 cfg.backbone = "convnext_small.fb_in22k_ft_in1k"
 cfg.ema = True
 cfg.ema_decay = 0.99
